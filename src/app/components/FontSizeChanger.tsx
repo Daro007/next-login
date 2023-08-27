a08a95ade01e24cc009a2e7c00c5eb269c152506
dario elias <darioelias@darios-iMac.local>
2023-08-27 17:30:48 +0200

    Font size use case

---
+++ src/app/components/FontSizeChanger.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface FontSizeChangerProps {
+  fontSizeOptions: number[];
+  selectedSize: number;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const FontSizeChanger: React.FC<FontSizeChangerProps> = ({ fontSizeOptions, selectedSize, onChange }) => {
+  return (
+    <div>
+      <label>Select font size: </label>
+      <select value={selectedSize} onChange={onChange}>
+        {fontSizeOptions.map(size => (
+          <option key={size} value={size}>{size}px</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FontSizeChanger;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import FontSizeChanger from "../components/FontSizeChanger";
 
 export default function Profile() {
   const router = useRouter();
   const [username, setUsername] = useState<string>("Username");
+
+  const [fontSize, setFontSize] = useState<number>(16);
+
+  const handleFontSizeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const newSize = parseInt(event.target.value);
+    setFontSize(newSize);
+  };
 
   useEffect(() => {
     const usernameLoggedIn = localStorage.getItem("usernameLoggedIn");
@@ -23,14 +33,20 @@ export default function Profile() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-lg">
-          <h1 className="mb-5 text-5xl font-bold">Welcome {username} !</h1>
+          <h1 className="mb-5 text-5xl font-bold" style={{ fontSize: `${fontSize}px` }}>Welcome {username} !</h1>
           <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          
-          <button type='submit' className="btn btn-error">Delete account </button>
+          <FontSizeChanger
+            fontSizeOptions={[12, 16, 20]}
+            selectedSize={fontSize}
+            onChange={handleFontSizeChange}
+          />
+          <button type="submit" className="btn btn-error">
+            Delete account{" "}
+          </button>
         </div>
       </div>
     </div>
